0dd0dc51daf080d4980a65037724ddd5190ea54c
ckapak <christinekapak@googlemail.com>
2019-10-21 16:16:32 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -12,20 +12,32 @@ console.log ("User flipped " + cardThree)
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
 
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log("User flipped " + cardOne);
-
-var cardTwo = cards[1];
-cardsInPlay.push(cardTwo)
-console.log("User flipped " + cardTwo);
-
-if (cardsInPlay.length === 2) {
+var checkForMatch = function() {
 
 	if (cardsInPlay[0] === cardsInPlay[1]) {
-		alert("You found a match!");
-	} else { 
-		alert("Sorry, try again.");
+  		console.log("You found a match!");
+	} else {
+		console.log("Sorry, try again.");
 	}
 
-} 
+}
+
+var flipcard = function(cardId) {
+	console.log("User flipped " + cards[cardId]);
+
+	cardsInPlay.push(cards[cardId]);
+
+	if (cardsInPlay.length === 2) {
+
+		if (cardsInPlay[0] === cardsInPlay[1]) {
+			alert("You found a match!");
+		} else { 
+			alert("Sorry, try again.");
+		}
+	}
+
+	checkForMatch()
+}
+
+flipcard(0);
+flipcard(2);
